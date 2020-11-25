e2de74b948cbac6748ca306c2f6f70a144d40f1f
23-Anjali <65702019+23-Anjali@users.noreply.github.com>
2020-11-25 12:26:20 +0530

    Add files via upload

---
+++ day 2 code.py
@@ -0,0 +1,28 @@
+# Ques 1  Creating the three variables a,b,c to same value of any integer and do the following
+#    divide a by 10
+  # multiply  b by 50
+  # add c value by 60
+a=30
+b=30
+c=30
+print(a/10)
+print(b*50)
+print(c+60)
+
+print("**********")   # for seperation
+
+# ques2 create a string variable of 5 characters and replace the 3rd charachter with G
+str = "sinfh"
+str= str.replace("f","g")
+print(str)
+print("**********")    # for seperation
+#Ques3 CREATE TWO values (a,b) of int and float and convert them vice versa
+a=6
+b=4.5
+f= float(a)
+print(f)
+n= int(b)
+print(n)
+
+
+print("**********")             # for seperation
