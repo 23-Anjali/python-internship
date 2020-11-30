1353c91ffc00bc50cb0c38dea27d937066b107a5
23-Anjali <65702019+23-Anjali@users.noreply.github.com>
2020-11-30 18:56:03 +0530

    Add files via upload

---
+++ day 7  another file.py
@@ -0,0 +1,2 @@
+import day7
+day7.my_module()
---
+++ day7.py
@@ -0,0 +1,27 @@
+
+#QUES 1 Create a pyhton module with list and import the module in another .py file and change the value in list
+def my_module():
+    list = [1, 2, 3, 4, 5, 6]
+    print(list)
+#QUES 2 Install pandas package (try to import package in a python file)
+
+#pip install pandas
+
+#QUES 3 Import a module the picks random number and write  a program to fetch a random number from 1to 100 for every run
+import random
+print(random.randint(1,100))
+
+#QUES 4 import sys package and find the python path.
+import sys
+print(sys.path)
+#QUES 5  Try to install a package and uninstall a package using pip
+#pip install <package name>
+#pip uninstall<package-name>
+
+
+
+
+
+
+
+
